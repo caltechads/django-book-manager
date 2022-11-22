a3a106c3e9fcfaec355918b24ecdff2e25cc4d97
Glenn Bach <glenn@caltech.edu>
2022-11-22 13:46:59 -0800

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name="django-book-manager",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(),
     include_package_data=True,
     package_data={'book_manager': ["py.typed"]},
